ee2f5ee71177cd9ddb82b1e65b0ac6bda18ce0ac
Meeran Husain <meeranhusain3@gmail.com>
2026-09-19 22:25:22 +0530

    Update home view to render template and add home.html file

--- Employee_main/settings.py
+++ Employee_main/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Employee_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],  # Specify the directory for your templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Employee_main/views.py
+++ Employee_main/views.py
@@ -6,5 +6,5 @@ from django.shortcuts import render
 def home(request):
     # fetch data from the Employee tables
     employees = Employee.objects.all()
-    print(employees)   # for debugging purposes, It's output will be displayed in the console where the Django server is running.
-    return HttpResponse("Welcome to the Employee Dashboard!")
+    # print(employees)   # for debugging purposes, It's output will be displayed in the console where the Django server is running.
+    return render(request, 'home.html')
