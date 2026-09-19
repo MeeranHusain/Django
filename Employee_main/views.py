0fbd557954073b7c3a1c7a8037431ba5aae5eb04
Meeran Husain <meeranhusain3@gmail.com>
2026-09-19 23:23:23 +0530

    Enhance home view and template to display employee list with Bootstrap styling

--- Employee_main/views.py
+++ Employee_main/views.py
@@ -7,7 +7,7 @@ def home(request):
     # fetch data from the Employee tables
     employees = Employee.objects.all()
     context = {
-        'employees': employees
+        'employees': employees,
     }
     # print(employees)   # for debugging purposes, It's output will be displayed in the console where the Django server is running.
     return render(request, 'home.html', context)
