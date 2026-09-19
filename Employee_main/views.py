afd6414db5b9c1c8d0a3b0e2f5d039b9dd287880
Meeran Husain <meeranhusain3@gmail.com>
2026-09-19 22:29:40 +0530

    Update home view to pass employee data to template for rendering

--- Employee_main/views.py
+++ Employee_main/views.py
@@ -6,5 +6,8 @@ from django.shortcuts import render
 def home(request):
     # fetch data from the Employee tables
     employees = Employee.objects.all()
+    context = {
+        'employees': employees
+    }
     # print(employees)   # for debugging purposes, It's output will be displayed in the console where the Django server is running.
-    return render(request, 'home.html')
+    return render(request, 'home.html', context)
